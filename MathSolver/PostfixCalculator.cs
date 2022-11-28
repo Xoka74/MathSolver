adff361b4442e358bfa0fbe009be8d9dd1d605b1
Xoka74 <ya.vladimir565@mail.ru>
2022-11-28 18:59:37 +0500

    ALMOST!!!

--- MathSolver/PostfixCalculator.cs
+++ MathSolver/PostfixCalculator.cs
@@ -19,19 +19,36 @@ namespace MathSolver
             var elements = postfixExpression.Split(' ');
             var elementStack = new List<BigIntComplex>();
             var operationStack = new List<string>();
+            var functionElementsStack = new List<BigIntComplex>();
             for (int i = 0; i < elements.Length; i++)
             {
+                Console.WriteLine($"i: {i}");
+                Console.WriteLine($"element: {elements[i]}");
+                Console.Write("element stack: ");
                 elementStack.PrintList();
+                Console.Write("operation stack: ");
                 operationStack.PrintList();
+                Console.Write("function stack: ");
+                functionElementsStack.PrintList();
                 //Console.WriteLine($"i: {i}");
                 //Console.WriteLine($"depthCount:{operationStack.Count}");
                 if (IsOperation(elements[i]))
                 {
-                    while (operationStack.Count > 0)
+                    if (functionElementsStack.Count == operationStack.Count && functionElementsStack.Count != 0 && operationStack.Count != 0)
                     {
-                        elementStack[^2] = PerformOperation(elementStack[^2], elementStack[^1], operationStack[^1]);
-                        elementStack.RemoveAt(elementStack.Count - 1);
-                        operationStack.RemoveAt(operationStack.Count - 1);
+                        functionElementsStack.Add(BigIntComplex.Parse(elements[i]));
+                    }
+                    else if (functionElementsStack.Count == 2 && operationStack.Count == 1)
+                    {
+                        while (operationStack.Count > 0)
+                        {
+                            functionElementsStack[^2] = PerformOperation(functionElementsStack[^2], functionElementsStack[^1], operationStack[^1]);
+                            functionElementsStack.RemoveAt(functionElementsStack.Count - 1);
+                            operationStack.RemoveAt(operationStack.Count - 1);
+                        }
+                        
+                        elementStack.Add(functionElementsStack[0]);
+                        functionElementsStack.RemoveAt(0);
                     }
 
                     if (elementStack.Count <= 1)
@@ -45,36 +62,54 @@ namespace MathSolver
                 else if (IsFunction(elements[i]))
                 {
                     if (IsFunction(elements[i + 1]))
-                    {
                         operationStack.Add(elements[i]);
-                    }
                     else if (IsFunction(elements[i + 2]))
                     {
                         operationStack.Add(elements[i]);
-                        elementStack.Add(BigIntComplex.Parse(elements[i + 1]));
+                        functionElementsStack.Add(BigIntComplex.Parse(elements[i + 1]));
                         i = i + 1;
                     }
                     else
                     {
                         var element1 = BigIntComplex.Parse(elements[i + 1]);
                         var element2 = BigIntComplex.Parse(elements[i + 2]);
-                        elementStack.Add(PerformOperation(element1, element2, elements[i]));
+                        var result = PerformOperation(element1, element2, elements[i]);
+
+                        if (operationStack.Count > 0)
+                        {
+                            functionElementsStack.Add(result);   
+                        }
+                        else
+                        {
+                            elementStack.Add(result);
+                        }
                         i = i + 2;
                     }
                 }
                 else
                 {
-
-                    while (operationStack.Count > 0)
+                    if (functionElementsStack.Count == operationStack.Count && functionElementsStack.Count != 0 && operationStack.Count != 0)
                     {
-                        elementStack[^2] = PerformOperation(elementStack[^2], elementStack[^1], operationStack[^1]);
-                        elementStack.RemoveAt(elementStack.Count - 1);
-                        operationStack.RemoveAt(operationStack.Count - 1);
+                        functionElementsStack.Add(BigIntComplex.Parse(elements[i]));
                     }
-                    elementStack.Add(BigIntComplex.Parse(elements[i]));
+                    else if (functionElementsStack.Count == 2 && operationStack.Count == 1)
+                    {
+                        while (operationStack.Count > 0)
+                        {
+                            functionElementsStack[^2] = PerformOperation(functionElementsStack[^2], functionElementsStack[^1], operationStack[^1]);
+                            functionElementsStack.RemoveAt(functionElementsStack.Count - 1);
+                            operationStack.RemoveAt(operationStack.Count - 1);
+                        }
+                        
+                        elementStack.Add(functionElementsStack[0]);
+                        functionElementsStack.RemoveAt(0);
+                        elementStack.Add(BigIntComplex.Parse(elements[i]));
+                    }
+                    else
+                        elementStack.Add(BigIntComplex.Parse(elements[i]));
                 }
 
-                Console.WriteLine("-----------------------");
+                // Console.WriteLine("-----------------------");
             }
 
             if (elementStack.Count == 2)
